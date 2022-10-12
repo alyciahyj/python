ee71d7f3d27cdba713f8840bcf980e3a01a2a7a2
alyciahyj <alyciahyj@gmail.com>
2022-10-12 20:11:23 +0800

    class3 try

---
+++ class2/class/class.py
@@ -0,0 +1,27 @@
+'''
+max(1,2,3)
+len('22')
+type(True)
+int(123)
+str(123.)
+float(456)
+bool('1234')
+print(max(1,2,3))
+a = input()
+print(type(a))
+print(a)
+hi+no*5
+print("輸入內容是")
+print("透過input輸入資料型態為)
+print("可以把輸入的東西從起來")
+a = input("這是提示使用者輸入的文字，請輸入:")
+print(a)
+
+a = input("請輸入你年齡:")
+print('你的年齡是:'+a+ '歲')
+'''
+r=input("請輸入半徑:")
+r=int(r)
+pi=3.14
+area=pi*r**2
+print(area)
