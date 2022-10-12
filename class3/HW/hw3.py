a865f5f6840e065d9b5477a8330ae56728cc74b4
alyciahyj <alyciahyj@gmail.com>
2022-10-12 20:33:10 +0800

    hw3

--- class3/HW/hw3.py
+++ class3/HW/hw3.py
@@ -14,3 +14,10 @@ git add .
 git commit -m "提示文字"
 git push
 """
+try:
+    f = float(input("請輸入華氏溫度:"))
+except:
+    print('發生錯誤')
+else:
+    c = 5 / 9 * (f - 32)
+    print(f"華氏溫度{f}F等於攝氏溫度{c}C")
