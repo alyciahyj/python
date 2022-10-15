505b1e028f6f7fb7b77288aa6941887352acad93
alyciahyj <alyciahyj@gmail.com>
2022-10-15 15:21:45 +0800

    class4+hw4

---
+++ class4/class/class.py
@@ -0,0 +1,44 @@
+'''
+not False
+not True
+not 1 == 1
+not not False
+
+True and True
+True and False
+False and True
+False and False
+
+True or True  
+True or False
+False or True
+False or  False
+'''
+
+# password = input("請輸入密碼:")
+# if password == "107138":
+#     print("welcome yue jia")
+# elif password == "110166":
+#     print("welcome shi yun")
+# else:
+#     print("beep beep")
+# print("beep beep")
+
+# password = int(input("請輸入成績"))
+# if password > 90:
+#     print("A")
+# elif password > 80:
+#     print("B")
+# elif password > 70:
+#     print("C")
+# elif password > 60:
+#     print("D")
+# elif password < 60:
+#     print("E")
+'''
+password = int(input("請輸入數字"))
+if password % 2 == 0:
+    print("是偶數")
+else:
+    print("是奇數")
+'''
